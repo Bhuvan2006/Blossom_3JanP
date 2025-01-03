4d60147ff8cd079b6fe498b65e3d5d57c028ffde
Bhuvan N R <bhuvannr2006@gmail.com>
2025-01-03 21:38:36 +0530

    Add files via upload
    adding all changes made till jan3 to fully deployable

---
+++ FTD project-1/main.js
@@ -0,0 +1,18 @@
+function showslider(){
+  const slider=document.querySelector('.slider');
+  slider.style.display='flex';
+}
+function closeslider(){
+ const slider=document.querySelector('.slider');
+ slider.style.display='none';
+}
+
+
+const tw=document.querySelector(".typewrite")
+text_shown =["Welcome","To","Let's Travel"]
+charidx=0
+caridx=0
+tw.innerHTML=`<h1 style:"color:'yellow' "> Welcome </h1><h1>To Blossom</h1>
+
+
+`
